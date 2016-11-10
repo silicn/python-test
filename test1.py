f3545b9f856ef4879891efd11a8e4331ae646be2
slicn <silicn@126.com>
2016-11-10 18:30:14 +0800

    add first

---
+++ test1.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import math
+
+# for i in xrange(10000):
+# 	#转化为整型值
+# 	x = int(math.sqrt(i + 100))
+# 	y = int(math.sqrt(i + 268))
+
+# 	if (x * x == i + 100) and (y * y == i+268):
+# 		pass
+# 		print i 
+
+
+
+
+# year  = int(raw_input('year:\n'))
+# month = int(raw_input('month:\n'))
+# day = int(raw_input('day:\n'))
+
+# months = (0,31,59,90,120,151,181,212,243,273,304,334)
+
+# if 0 <month <= 12:
+# 	sum = months[month - 1]
+# else:
+# 	print 'data error'
+
+# sum += day
+
+# leap = 0
+
+# if (year %400 == 0) or ((year%4) == 0 and (year%100 != 0)):
+# 	leap = 1
+# if (leap == 1) and (month > 2):
+#     sum += 1
+# print 'it is the %dth day.' % sum   
+
+
+
+# l = []
+
+# for i in xrange(3):
+# 	x = int(raw_input('interger:\n'))
+# 	l.append(x)
+# l.sort()
+# print l
+
+
+
+
+
+
+
+
+
