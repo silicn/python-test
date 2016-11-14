1ac503543d77f7f7b14d526d44f3d98125d8bafa
slicn <silicn@126.com>
2016-11-14 17:42:07 +0800

    test

--- test1.py
+++ test1.py
@@ -3,6 +3,9 @@
 
 import math
 
+import time
+
+
 # for i in xrange(10000):
 # 	#转化为整型值
 # 	x = int(math.sqrt(i + 100))
@@ -48,8 +51,95 @@ import math
 
 
 
+# def fib(n):
+# 	a,b = 1,1
+# 	for i in range(n-1):
+# 		a,b = b,a+b
+# 	return a
+
+# # 输出了第10个斐波那契数列
+# print fib(10)
 
 
+# a = [1,2,3]
+# b = a[:]
+# print b 
+
+
+
+# for i in range(1,10):
+#     for j in range(1,10):
+#         result = i * j
+#         print '%d * %d = % -3d' % (i,j,result)
+#     pass
+
+
+# import time
+
+# myD = {1: 'a', 2: 'b'}
+# for key, value in dict.items(myD):
+# 	print key, value
+# 	time.sleep(1) # 暂停 1 秒
+
+
+
+# f1 = 1
+# f2 = 1
+# for i in range(1,21):
+#     print '%12ld %12ld' % (f1,f2),
+#     if (i % 3) == 0:
+#         print ''
+#     f1 = f1 + f2
+#     f2 = f1 + f2
+
+
+
+if __name__ == '__main__':
+	a = 077
+	b = a & 3
+	print 'a & b  = %d' % b
+
+	b &= 7
+
+	print 'a & b = %d' % b
+
+pass
+
+list1 = ['physics','chemistry',1997,2000]
+list2 = [1,2,3,4,5,6,7]
+
+print "list1[0] :",list1[0]
+
+print "list2[1:5]",list2[1:5]
+
+
+pass
+
+tup1 = (12,34.56)
+
+tup2 = ('abc','xyz')
+
+tup3 = tup1 + tup2
+
+print tup3
+
+pass
+
+dict = {'alice':'1234','beth':'5678','cecil':'2738'}
+print dict
+
+ticks = time.time()
+print "当前时间为：",ticks
+
+localtime = time.asctime(time.localtime(time.time()))   
+
+print "本地时间：",localtime
+
+def test(str):
+	print str 
+	return
+
+test("设么东西")
 
 
 
