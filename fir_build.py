eef1b86878e95696b99d9f01ccdba24a4899b732
slicn <silicn@126.com>
2016-11-16 17:16:05 +0800

    add

--- fir_build.py
+++ fir_build.py
@@ -4,19 +4,16 @@
 import subprocess
 
 fir_token = "454213ff6ed0b48cdd56fdc751977cdc"
-
 out_put_path = "Users/JiaHao/Desktop/build/"
-
 
 def buildProject():
     log =  raw_input('输入build log:')
     buildCmd = 'fir build_ipa /Users/JiaHao/Desktop/YunKeAssistant/YunKeAssistant1.0.1 -o %s -p -c "%s" -Q -T %s' %(out_put_path,log,fir_token)
     process = subprocess.Popen(buildCmd, shell = True)
-	# process.wait()
-    print "build xcode ..."
+	process.wait()
+    print "success"
 
 def main():
     buildProject()
-
 if __name__ == '__main__':
 	main()
